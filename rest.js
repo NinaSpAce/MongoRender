ccecd9ebd2fcf7be4c631fee8a3887db31a67b9e
Christina Hoover <89437566+NinaSpAce@users.noreply.github.com>
2023-04-07 17:10:42 -0500

    Update rest.js

--- rest.js
+++ rest.js
@@ -9,23 +9,40 @@ var fs = require('fs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-var data = {
-    'id': id,
-    'creation': creation,
-    'updated': updated,
-    'type': type,
-    'subject': subject,
-    'description': description,
-    'priority': priority,
-    'status': state,
-    'recipient': recipient,
-    'submitter': submitter,
-    'assignee_id': assignee_id,
-    'followers_ids': followers_ids,
-    'tags': tags,        
-}
+
 router.get('/rest/list/', function(req,res){
     res.send('GET all tickets.');
+    const id = req.body.id;
+    const creation = req.body.creation;
+    const updated = req.body.updated;
+    const type = req.body.type;
+    const subject = req.body.subject;
+    const description = req.body.description;
+    const priority = req.body.priority;
+    const status = req.body.state;
+    const recipient = req.body.recipient;
+    const submitter = req.body.submitter;
+    const assignee_id= req.body.assignee_id;
+    const followers_ids = req.body.followers_ids;
+    const tags = req.body.tags;
+
+
+    var data = {
+        'id': id,
+        'creation': creation,
+        'updated': updated,
+        'type': type,
+        'subject': subject,
+        'description': description,
+        'priority': priority,
+        'status': state,
+        'recipient': recipient,
+        'submitter': submitter,
+        'assignee_id': assignee_id,
+        'followers_ids': followers_ids,
+        'tags': tags,        
+    }
+
     fs.readFile("tickets.txt", function(err,data){
         if (err){
             console.log(err);
@@ -59,6 +76,22 @@ router.post('/rest/ticket/', function(req,res){
     const followers_ids = req.body.followers_ids;
     const tags = req.body.tags;
 
+
+    var data = {
+        'id': id,
+        'creation': creation,
+        'updated': updated,
+        'type': type,
+        'subject': subject,
+        'description': description,
+        'priority': priority,
+        'status': state,
+        'recipient': recipient,
+        'submitter': submitter,
+        'assignee_id': assignee_id,
+        'followers_ids': followers_ids,
+        'tags': tags,        
+    }
 
     var JSONdata = JSON.stringify(data);
 
