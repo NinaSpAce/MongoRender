1c4da280eab9d1cb995c41f8bb3cbb79aab0458f
Christina Hoover <89437566+NinaSpAce@users.noreply.github.com>
2023-04-07 17:08:26 -0500

    Update rest.js

--- rest.js
+++ rest.js
@@ -9,9 +9,35 @@ var fs = require('fs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+var data = {
+    'id': id,
+    'creation': creation,
+    'updated': updated,
+    'type': type,
+    'subject': subject,
+    'description': description,
+    'priority': priority,
+    'status': state,
+    'recipient': recipient,
+    'submitter': submitter,
+    'assignee_id': assignee_id,
+    'followers_ids': followers_ids,
+    'tags': tags,        
+}
 router.get('/rest/list/', function(req,res){
     res.send('GET all tickets.');
+    fs.readFile("tickets.txt", function(err,data){
+        if (err){
+            console.log(err);
+        }
+        else{
+            console.log("File read successfully! \n");
+            console.log("Contents of file now:\n");
+            res.send(data)
+        }
+       
+
+    });
   });
 router.get('/rest/ticket/:id', function(req,res){
     res.send('GET a single ticket');
@@ -26,28 +52,14 @@ router.post('/rest/ticket/', function(req,res){
     const subject = req.body.subject;
     const description = req.body.description;
     const priority = req.body.priority;
-    const status = req.body.status;
+    const status = req.body.state;
     const recipient = req.body.recipient;
     const submitter = req.body.submitter;
     const assignee_id= req.body.assignee_id;
     const followers_ids = req.body.followers_ids;
     const tags = req.body.tags;
 
-    var data = {
-        'id': id,
-        'creation': creation,
-        'updated': updated,
-        'type': type,
-        'subject': subject,
-        'description': description,
-        'priority': priority,
-        'status': status,
-        'recipient': recipient,
-        'submitter': submitter,
-        'assignee_id': assignee_id,
-        'followers_ids': followers_ids,
-        'tags': tags,        
-    }
+
     var JSONdata = JSON.stringify(data);
 
     fs.writeFile("tickets.txt", JSONdata, function(err){
