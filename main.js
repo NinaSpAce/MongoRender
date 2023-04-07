4891d8012e498f01b71479531b7786a46e23f3a4
Christina Hoover <89437566+NinaSpAce@users.noreply.github.com>
2023-04-07 17:34:29 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const { MongoClient } = require("mongodb");
-const tickets = require('./rest.js')
 
 
 
@@ -8,10 +7,11 @@ const uri = "mongodb+srv://classuser:YUriCVW1ndtLOIpO@cmps-415.joavhvm.mongodb.n
 
 // --- This is the standard stuff to get it to work on the browser
 const express = require('express');
+const bodyParser = require('body-parser');
+var fs = require('fs');
 const app = express();
 const port = 3000;
 app.listen(port);
-app.use('/rest', tickets);
 console.log('Server started at http://localhost:' + port);
 
 app.use(express.json());
@@ -29,6 +29,103 @@ app.get('/', function(req, res) {
 app.get('/say/:name', function(req, res) {
   res.send('Hello ' + req.params.name + '!');
 });
+
+app.get('/rest/list/', function(req,res){
+    res.send('GET all tickets.');
+    const id = req.body.id;
+    const creation = req.body.creation;
+    const updated = req.body.updated;
+    const type = req.body.type;
+    const subject = req.body.subject;
+    const description = req.body.description;
+    const priority = req.body.priority;
+    const status = req.body.state;
+    const recipient = req.body.recipient;
+    const submitter = req.body.submitter;
+    const assignee_id= req.body.assignee_id;
+    const followers_ids = req.body.followers_ids;
+    const tags = req.body.tags;
+
+
+    var data = {
+        'id': id,
+        'creation': creation,
+        'updated': updated,
+        'type': type,
+        'subject': subject,
+        'description': description,
+        'priority': priority,
+        'status': state,
+        'recipient': recipient,
+        'submitter': submitter,
+        'assignee_id': assignee_id,
+        'followers_ids': followers_ids,
+        'tags': tags,        
+    }
+
+    fs.readFile("tickets.txt", function(err,data){
+        if (err){
+            console.log(err);
+        }
+        else{
+            console.log("File read successfully! \n");
+            console.log("Contents of file now:\n");
+            res.send(data)
+        }
+       
+
+    });
+  });
+app.get('/rest/ticket/:id', function(req,res){
+    res.send('GET a single ticket');
+    res.send('The ticket' + req.params.id + 'is: ');
+  });
+app.post('/rest/ticket/', function(req,res){
+    res.send('CREATE a new ticket');
+    const id = req.body.id;
+    const creation = req.body.creation;
+    const updated = req.body.updated;
+    const type = req.body.type;
+    const subject = req.body.subject;
+    const description = req.body.description;
+    const priority = req.body.priority;
+    const status = req.body.state;
+    const recipient = req.body.recipient;
+    const submitter = req.body.submitter;
+    const assignee_id= req.body.assignee_id;
+    const followers_ids = req.body.followers_ids;
+    const tags = req.body.tags;
+
+
+    var data = {
+        'id': id,
+        'creation': creation,
+        'updated': updated,
+        'type': type,
+        'subject': subject,
+        'description': description,
+        'priority': priority,
+        'status': state,
+        'recipient': recipient,
+        'submitter': submitter,
+        'assignee_id': assignee_id,
+        'followers_ids': followers_ids,
+        'tags': tags,        
+    }
+
+    var JSONdata = JSON.stringify(data);
+
+    fs.writeFile("tickets.txt", JSONdata, function(err){
+        if (err){
+            console.log(err);
+        }
+        else{
+            console.log("File written successfully\n");
+        }
+    });
+});
+
+
 
 
 // Route to access database:
