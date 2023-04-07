730e30cb7f3b707c1fcaa514e749f441d3704d30
Christina Hoover <89437566+NinaSpAce@users.noreply.github.com>
2023-04-07 16:49:10 -0500

    Update and rename testmongo.js to main.js

---
+++ main.js
@@ -0,0 +1,62 @@
+var express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+const router = express.Router();
+var fs = require('fs');
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+router.get('/rest/list/', function(req,res){
+    res.send('GET all tickets.');
+  });
+router.get('/rest/ticket/:id', function(req,res){
+    res.send('GET a single ticket');
+    res.send('The ticket' + req.params.id + 'is: ');
+  });
+router.post('/rest/ticket/', function(req,res){
+    res.send('CREATE a new ticket');
+    const id = req.body.id;
+    const creation = req.body.creation;
+    const updated = req.body.updated;
+    const type = req.body.type;
+    const subject = req.body.subject;
+    const description = req.body.description;
+    const priority = req.body.priority;
+    const status = req.body.status;
+    const recipient = req.body.recipient;
+    const submitter = req.body.submitter;
+    const assignee_id= req.body.assignee_id;
+    const followers_ids = req.body.followers_ids;
+    const tags = req.body.tags;
+
+    var data = {
+        'id': id,
+        'creation': creation,
+        'updated': updated,
+        'type': type,
+        'subject': subject,
+        'description': description,
+        'priority': priority,
+        'status': status,
+        'recipient': recipient,
+        'submitter': submitter,
+        'assignee_id': assignee_id,
+        'followers_ids': followers_ids,
+        'tags': tags,        
+    }
+    var JSONdata = JSON.stringify(data);
+
+    fs.writeFile("tickets.txt", JSONdata, function(err){
+        if (err){
+            console.log(err);
+        }
+        else{
+            console.log("File written successfully\n");
+        }
+    });
+});
+module.exports = router;
