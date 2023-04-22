9f5dfd83f258fa50a72fdb68503bb9d167183473
Christina Hoover <89437566+NinaSpAce@users.noreply.github.com>
2023-04-21 19:03:22 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 
-// --- This is the standard stuff to get it to work on the browser
+const { MongoClient } = require("mongodb");
+
+// The uri string must be the connection string for the database (obtained on Atlas).
+const uri = "mongodb+srv://classuser:LJ6fvgWHY1H4eJ5C@cmps-415.joavhvm.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    const db = client.db('CMPS415');
+    const collection = db.collection('atlas');
+ 
 const express = require('express');
 const bodyParser = require('body-parser');
 var fs = require('fs');
@@ -22,77 +33,125 @@ app.get('/', function(req, res) {
   res.send(outstring);
 });
 
+//form
+app.get('/form', function(req, res){
+res.setHeader('Content-Type', 'text/html');
+fs.readFile('./form.html', 'utf8', (err, contents) => {
+  if(err) {
+      console.log('Form file Read Error', err);
+      res.write("<p>Form file Read Error");
+  } else {
+      console.log('Form has been loaded\n');
+      res.write(contents + "<br>");
+  }
+  res.end();
+});
+});
 
-
+//GET all tickets
 app.get('/rest/list/', function(req,res){
-
-    fs.readFile("./tickets.json", 'utf8', (err,jsonString) => {
-        if (err){
-            console.error(err);
-          return;
-        }
-        try{
-          const tickets = JSON.parse(jsonString);
-            console.log("File read successfully!");
-            console.log("Contents of file now: ");
-            res.json(tickets);
-        }
-        catch (err) {
-          console.error(err);
-        }
-    });
-  });
-app.get('/rest/ticket/:id', function(req,res){
- const id = parseInt(req.params.id);
-    console.log('Looking for: ' + id);
-  
-     fs.readFile("./tickets.json", 'utf8', (err,jsonString) => {
-        if (err){
-            console.error(err);
-          return;
-        }
-          try{
-            const tickets = JSON.parse(jsonString);
-            const ticket = tickets.find(t => t.id === id);
-            if (ticket){
-              console.log(`Ticket with ID ${id} found!`);
-              res.json(ticket);
-            }
-            else{
-            console.log(`Ticket with ID ${id} not found!`);
-            res.status(404);
-            }
-          }
-          catch(err){
-            console.error(err);
-          }
-       });
-  });
-app.post('/rest/ticket/', function(req,res){
-   const data = req.body;
-   fs.readFile("./tickets.json", 'utf8', (err,jsonString) => {
-    if (err){
-        console.error(err);
+  collection.find({}).toArray(function(err, docs) {
+    if (err) {
+      console.error('Error querying MongoDB:', err);
+      res.status(500);
       return;
     }
-    try{
-      const currentdata = JSON.parse(jsonString);
-      currentdata.push(data);
-    
-    fs.writeFile("./tickets.json", JSON.stringify(currentdata, null, 2), (err) => {
-        if (err){
-            console.error(err);
-            res.status(500);
-        }
-        else{
-            console.log("File written successfully.");
-            res.status(200).json(data);
-        }
-    });
-  }
-  catch(err){
-    console.error(err);
-    res.status(500).send("Error parsing JSON data!");
-  }
+    else {
+      console.log('Tickets retrieved!\n');
+    }
+    res.setHeader('Content-Type', 'application/json');
+    res.send(docs);
+  });
+  });
+
+//GET by id
+app.get('/rest/list/:id', function(req,res){
+ const id = parseInt(req.params.id);
+    console.log('Looking for: ' + id);
+    collection.findOne({ id: id }, function(err, doc) {
+      if (err) {
+        console.error('Could not find ID in MongoDB.', err);
+        res.status(500);
+        return;
+      }
+      else {
+        console.log('Ticket found!\n');
+      }
+    res.setHeader('Content-Type', 'application/json');
+    res.send(doc);
+  });
+});
+
+//POST ticket
+app.post('/rest/ticket/', function(req,res){
+  const ticket = {
+    id: req.body.id,
+    created_at: req.body.created_at,
+    updated_at: req.body.updated_at,
+    type: req.body.type,
+    subject: req.body.subject,
+    description: req.body.description,
+    priority: req.body.priority,
+    status: req.body.status,
+    recipient: req.body.recipient,
+    submitter: req.body.submitter,
+    assignee_id: req.body.assignee_id,
+    follower_ids: req.body.follower_ids
+  };
+  collection.insertOne(ticket, function(err) {
+    if (err) {
+      console.error('Could not insert the ticket.', err);
+      res.status(500);
+      return;
+    }
+    else {
+      console.log('Ticket added!\n');
+    }
+    res.redirect('/rest/list/');
+ 
 });
 });
+
+//UPDATE ticket
+app.put('/rest/ticket/:id',function(req){
+  const id = parseInt(req.params.id);
+    console.log('Updating ticket with ID: ' + id);
+
+  const form = document.querySelector('#ticket-form');
+  const formData = new FormData(form);
+
+
+  fetch(`/rest/ticket/${id}`, {
+    method: 'PUT',
+    body: formData
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Ticket updated:', data);
+  })
+  .catch(error => console.error('Error updating ticket:', error));
+});
+
+//DELETE ticket
+app.delete('/rest/ticket/delete/:id', function(req,res){
+  const id = parseInt(req.params.id);
+  console.log('Deleting ticket with ID: ' + id);
+  collection.deleteOne({id: new ObjectId(id)}, function(err, result) {
+    if (err) {
+      console.error('Could not delete the ticket.', err);
+      res.status(500);
+      return;
+    }
+    else {
+      console.log('Ticket deleted!\n');
+  }
+})
+});
+
+}
+finally {
+  // Ensures that the client will close when you finish/error
+  await client.close();
+}
+}
+run().catch(console.dir);
